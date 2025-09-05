8ec0eba2f6bbbb2f22909c9f31dd0f21499ed006
nevena <dnevena27@gmail.com>
2025-09-05 13:35:51 +0300

    modify api response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -2,11 +2,23 @@ import { test, expect } from "@playwright/test";
 import tags from "../test-data/tags.json";
 
 test.beforeEach(async ({ page }) => {
-  //So to create a mock we need to type command await page dot route and inside of the route.
-  //First argument we provide the URL that we want to intercept Go into the networking tab and first URL.
   await page.route("*/**/api/tags", async (route) => {
     await route.fulfill({
       body: JSON.stringify(tags),
+    });
+  });
+
+  await page.route("*/**/api/articles*", async (route) => {
+    //1.get the response
+    const response = await route.fetch();
+    const responseBody = await response.json();
+    //2.update the response body
+    responseBody.articles[0].title = "This is a test title";
+    responseBody.articles[0].description = "This is a description";
+
+    //3.fulfill the modified response as a desired response to the application.
+    await route.fulfill({
+      body: JSON.stringify(responseBody),
     });
   });
 
@@ -19,4 +31,10 @@ test.beforeEach(async ({ page }) => {
 
 test("has title", async ({ page }) => {
   await expect(page.locator(".navbar-brand")).toHaveText("conduit");
+  await expect(page.locator("app-article-list h1").first()).toContainText(
+    "This is a test title"
+  );
+  await expect(page.locator("app-article-list p").first()).toContainText(
+    "This is a description"
+  );
 });
