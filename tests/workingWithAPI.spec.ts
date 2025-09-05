ed25ba05bb5343ddea7bcc5952715cad327fcc35
nevena <dnevena27@gmail.com>
2025-09-05 11:25:05 +0300

    first test done

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://conduit.bondaracademy.com/");
+});
+
 test("has title", async ({ page }) => {
   await page.goto("https://conduit.bondaracademy.com/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
 });
