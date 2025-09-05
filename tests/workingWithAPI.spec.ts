83579c6a64ef6dc96ef27cc6c7958205c0dfff22
nevena <dnevena27@gmail.com>
2025-09-05 12:30:52 +0300

    add mocking api

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,8 +1,18 @@
 import { test, expect } from "@playwright/test";
+import tags from "../test-data/tags.json";
+
+test.beforeEach(async ({ page }) => {
+  //So to create a mock we need to type command await page dot route and inside of the route.
+  //First argument we provide the URL that we want to intercept Go into the networking tab and first URL.
+  await page.route("*/**/api/tags", async (route) => {
+    await route.fulfill({
+      body: JSON.stringify(tags),
+    });
+  });
+
+  await page.goto("https://conduit.bondaracademy.com/");
+});
 
 test("has title", async ({ page }) => {
-  await page.goto("https://conduit.bondaracademy.com/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.locator(".navbar-brand")).toHaveText("conduit");
 });
